b9cd97f2eae1c78d4705198d0aa6ae97c41639ab
Emerson Laurentino <emerson@laurentino.co>
2021-09-17 03:07:45 -0300

    solution for if2

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -9,8 +9,10 @@
 pub fn fizz_if_foo(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "literally anything" {
+        "baz"
     } else {
-        1
+        "bar"
     }
 }
 
